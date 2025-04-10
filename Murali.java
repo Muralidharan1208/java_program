954a4496e6941b67de180b9046cdd5a663d66476
Muralidharan1208 <136390511+Muralidharan1208@users.noreply.github.com>
2025-04-10 17:59:04 +0530

    Add files via upload

---
+++ Murali.java
@@ -0,0 +1,6 @@
+public class Murali{
+    public static void main(String[] args) {
+        System.out.println("The file name and class name must be the same....!!");
+        System.out.println("The file name must be named .java , to declare it as a java source file....!!");
+    }
+}
