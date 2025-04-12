1e7546a6fa27b43d8905b1942ac9615497d0a437
Muralidharan <muralidharan@crytonixtech.com>
2025-04-12 12:04:04 +0530

    Moved the file from python_program to python_program\java_program and Adding Welcome.java to the repository main branch through cmd prompt

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome{
+    public static void main(String[] args) {
+        
+        System.out.println("Hi, Associate Developer");
+
+        System.out.println("Keep practicing");
+
+        String statement = "All the best for your  Associate level oracle java certification";
+
+        System.out.println(statement);
+    }
+}
