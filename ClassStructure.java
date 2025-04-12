33f084c478172522bb45b0d4c315cd7a02e7edc3
Muralidharan <muralidharan@crytonixtech.com>
2025-04-12 12:17:50 +0530

    adding ClassStructure.java to the main  repository

---
+++ ClassStructure.java
@@ -0,0 +1,9 @@
+public class ClassStructure{
+    String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String userName) {
+        name = userName;
+    }
+}
